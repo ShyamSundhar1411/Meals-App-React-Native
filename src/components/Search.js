989cb4673ecb707127f051da93004e4556654340
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-06-11 16:30:54 +0530

    Search Feature

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Searchbar } from "react-native-paper";
 
 import { LocationContext } from "../services/location/location.context";
@@ -6,6 +6,10 @@ import { LocationContext } from "../services/location/location.context";
 export const SearchBarComponent = () => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  useEffect(() => {
+    search(searchKeyword);
+  }, [search, searchKeyword]);
   return (
     <Searchbar
       placeholder="Search for a location"
@@ -14,7 +18,6 @@ export const SearchBarComponent = () => {
         search(searchKeyword);
       }}
       onChangeText={(text) => {
-        console.log(text);
         setSearchKeyword(text);
       }}
     />
