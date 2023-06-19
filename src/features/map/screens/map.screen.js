2fb3e42156d82dec0b302cd672e3004f0494d4ce
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-06-19 23:46:02 +0530

    Marker

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
 import { SearchBarComponent } from "../components/map_search_component";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurant.context";
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
-export const MapScreen = () => (
-  <>
-    <SearchBarComponent />
-    <Map />
-  </>
-);
+export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+  console.log(restaurants);
+  const [latDelta, setLatDelta] = useState(0);
+  const { lat, lng, viewport } = location;
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+  return (
+    <>
+      <SearchBarComponent />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
+    </>
+  );
+};
